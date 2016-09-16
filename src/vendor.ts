803a6c1e283857268150cb3717a9ff41222b3ce5
Joy.M.Tang <Joy.M.Tang@newegg.com>
2016-09-16 16:09:11 +0800

    common angular frame

---
+++ src/vendor.ts
@@ -0,0 +1,10 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/common';
+import '@angular/core';
+import '@angular/http';
+import "@angular/forms";
+import '@angular/router';
+// RxJS
+import 'rxjs';
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+let webpack = require("webpack");
+let htmlWebpackPlugin = require("html-webpack-plugin");
+let extractTextPlugin = require("extract-text-webpack-plugin");
+let path = require("path");
+
+module.exports = {
+    entry: {
+        "vendor": "./src/vendor.ts",
+        "polyfills": "./src/polyfills.ts",
+        "app": "./src/main.ts"
+    },
+    output: {
+        path: "./dist",
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts', ".css"]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ["ts", "angular2-template"]
+            },
+            {
+                test: /\.css$/,
+                include: [
+                    path.resolve(__dirname, "src/assets/css"),
+                ],
+                loader: extractTextPlugin.extract("style","css?sourceMap")
+            },
+            {
+                test: /\.css$/,
+                include: [
+                    path.resolve(__dirname, "src/app")
+                ],
+                loader: "raw"
+            },
+            {
+                test: /\.html$/,
+                loader: "html"
+            },
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ["app", "vendor", "polyfills"]
+        }),
+        new extractTextPlugin("assets/css/[name].css"),
+        new htmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+}
